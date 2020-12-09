8406df8568ae94486581bc40a36c54593c123899
Giancarlo Carvajal <ftapmapas@gmail.com>
2020-12-09 04:35:44 -0500

    'actualizaciones'

--- src/app/component/shopping-product-list/shopping-product-list.component.ts
+++ src/app/component/shopping-product-list/shopping-product-list.component.ts
@@ -1,6 +1,8 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ShoppingProduct } from 'src/app/domain/shopping-product';
+import { CartService } from 'src/app/service/cart.service';
 import { ShoppingProductServiceService } from 'src/app/service/shopping-product-service.service';
 
 @Component({
@@ -14,7 +16,12 @@ export class ShoppingProductListComponent implements OnInit {
   public shoppingProducts:ShoppingProduct[];
   public total:number;
 
+  public showMsg:boolean= false;
+  public messages:string[];
+
+
   constructor(public shoppingProductService:ShoppingProductServiceService,
+              public cartService:CartService,
               public router:Router) { }
 
   ngOnInit(): void {
@@ -24,9 +31,6 @@ export class ShoppingProductListComponent implements OnInit {
   findAll():void{
     this.shoppingProductService.findAll().subscribe(data=>{
       this.shoppingProducts = data.filter(t=>t.carId === Number(localStorage.getItem("carId")));
-      for(let x of this.shoppingProducts){
-        this.total += x.total;
-      }
     })
   }
 
@@ -36,6 +40,14 @@ export class ShoppingProductListComponent implements OnInit {
 
   buy():void{
     this.router.navigate(["/buy"]);
+  }
+
+  clear():void{
+    this.cartService.clearCart(Number(localStorage.getItem("carId"))).subscribe(data=>{
+      this.messages=[""];
+      this.showMsg =true;
+      this.messages[0]="se vacio el carrito de compras actualiza para ver los cambios"; 
+    },err=>{})
   }
 
 }
